e49252b842f36138718160982945a6791b4c9c15
Jesse <jesarmarcin@gmail.com>
2022-02-28 16:41:45 +0300

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import "./global.css"
-const reactContentRoot = document.getElementById('root')
-
-ReactDOM.render(<App />, reactContentRoot);
-
